f448b2c93e8eb98478be735ebba86e9dfbae053c
Rakshit <prabhurakshit@gmail.com>
2020-03-18 16:16:17 +0530

    (incomplete)graphs using adjacency

--- graph_list.cpp
+++ graph_list.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 
-void addEdge(vector<int> adj[], int u, int v) 
+void addEdge(vector<int> arr[], int u, int v) 
 { 
-    adj[u].push_back(v); 
+    arr[u].push_back(v); //put element in that vertices's list
    // adj[v].push_back(u); for undirected but gotta make changes other places too 
 } 
   
@@ -27,13 +27,13 @@ void add(vector<int>arr[],int v)
   {
     int links;
  	cout<<"how many edges linked : ";
- 	cin>>links;
+ 	cin>>links;  //how many edges does this vertice have?
 	cout<<"enter them : ";
  	for(int i=0;i<links;i++)
  	 {	
 	   int inputlinks;
-	   cin>>inputlinks;
-	   addEdge(arr,j,inputlinks);
+	   cin>>inputlinks;  //vertices linked u input
+	   addEdge(arr,j,inputlinks); //j sent bcoz thats the vertice number
 	 }
   }
 }
@@ -47,4 +47,10 @@ int main()
 	add(arr,v);
     printGraph(arr, v); 
     return 0; 
-} 
+}
+
+/*
+this still incomplete though like:
+
+1)u can put any number of input edges inside a list even if they(vertices) dont exist
+*/
